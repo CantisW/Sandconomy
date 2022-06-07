dc7313424be40730a52055318c6364e57e642cc2
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-06-07 15:55:12 -0700

    feat: some dev debug tools

---
+++ src/commands/debug.ts
@@ -0,0 +1,12 @@
+import { CommandInteraction } from "discord.js";
+import { Discord, Slash } from "discordx";
+
+@Discord()
+export class Debug {
+    @Slash("debug", { "description": "Developer info about the server." })
+    async debug (
+        interaction: CommandInteraction
+    ) {
+        interaction.reply(`userId: ${interaction.user.id}\nchannelId: ${interaction.channelId}\nguildId: ${interaction.guildId}`)
+    }
+}
