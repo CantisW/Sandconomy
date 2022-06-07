79a52241b8eb8f8f6678271392ae66b1c3cca484
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-06-07 15:56:06 -0700

    feat: add ability to work for money

---
+++ src/commands/work.ts
@@ -0,0 +1,18 @@
+import { CommandInteraction } from "discord.js";
+import { Discord, Slash } from "discordx";
+import { createEmbed } from "../util/botUtils.js";
+import { work } from "../util/economyUtil.js";
+
+@Discord()
+export class Work {
+    @Slash("work", { "description": "Work at your job!" })
+    async work (
+        interaction: CommandInteraction
+    ) {
+        work(interaction.user.id).then(e => {
+                return interaction.reply({ embeds: [e]});
+            }).catch(e => {
+                return interaction.reply({ embeds: [e] });
+            })
+    }
+}
