42f7282c2b32db982227997c78aba8df5678964d
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-06-07 15:55:58 -0700

    feat: add ability to rob users

---
+++ src/commands/rob.ts
@@ -0,0 +1,47 @@
+import { CommandInteraction, User } from "discord.js";
+import { Discord, Permission, Slash, SlashGroup, SlashOption } from "discordx";
+import { getConfig, returnBotSetting } from "../util/botUtils.js";
+import { createRobMessage, rob } from "../util/economyUtil.js";
+
+const { currencyName } = getConfig();
+let ownerid = returnBotSetting("ownerId");
+
+if (returnBotSetting("environment") !== "production") {
+    ownerid = process.env.OWNERID || "";
+}
+
+@Discord()
+@SlashGroup({ name: "crime", description: "Do some dirty crime." })
+export class Rob {
+    @Slash("rob", { "description": "Rob money from another user." })
+    @SlashGroup("crime")
+    async rob (
+        @SlashOption("user", {"description": "Who? [ @User ]", "type": "USER" })
+        user: User,
+        interaction: CommandInteraction
+    ) {
+        rob(interaction.user.id, user.id).then(e => {
+            return interaction.reply({ embeds: [e] });
+        }).catch(e => {
+            return interaction.reply({ embeds: [e] });
+        })
+    }
+
+    @Slash("message", { "description": "Add a rob message." })
+    @SlashGroup("crime")
+    @Permission(false)
+    @Permission({ id: ownerid, type: "USER", permission: true })
+    async message (
+        @SlashOption("message", { "description": "The message.", "type": "STRING" })
+        @SlashOption("type", { "description": "If the rob is a positive or negative message.", "type": "BOOLEAN" })
+        message: string,
+        type: boolean,
+        interaction: CommandInteraction
+    ) {
+        if(await createRobMessage(message, type)) {
+            return interaction.reply("Successfully created rob message.")
+        } else {
+            return interaction.reply("Something has gone wrong.");
+        }
+    }
+}
