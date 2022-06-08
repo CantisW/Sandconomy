6c805d70deebf4a35bb0ceabe8cf089b7d34022a
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-06-08 16:35:49 -0700

    fix: make heads/tails a choice

--- src/commands/gamble.ts
+++ src/commands/gamble.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction } from "discord.js";
-import { Discord, Slash, SlashGroup, SlashOption } from "discordx";
+import { Discord, Slash, SlashChoice, SlashGroup, SlashOption } from "discordx";
 import { flipCoin } from "../util/economyUtil.js";
 
 @Discord()
@@ -8,15 +8,16 @@ export class Gamble {
     @Slash("flip", { description: "Flip a coin." })
     @SlashGroup("gamble")
     async flip (
-        @SlashOption("amount", { description: "The amount to wager." })
-        @SlashOption("choice", { description: "[ Heads | Tails ]", type: "STRING"})
+        @SlashOption("amount", { description: "How much to wager. [ string ]" })
         amount: string,
+        @SlashChoice("heads", "tails")
+        @SlashOption("choice", { description: "[ Heads | Tails ]", type: "STRING"})
         choice: string,
         interaction: CommandInteraction
     ) {
-        if (choice.toLowerCase() !== "heads") {
-            if (choice.toLowerCase() !== "tails") return interaction.reply("Please pick between heads or tails.");
-        }
+        // if (choice.toLowerCase() !== "heads") {
+        //     if (choice.toLowerCase() !== "tails") return interaction.reply("Please pick between heads or tails.");
+        // }
         flipCoin(interaction.user.id, amount, choice).then(e => {
             return interaction.reply({ embeds: [e] });
         }).catch(e => {
