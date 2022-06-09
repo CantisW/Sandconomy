260b98bbc08a5769302e045210cacff875288742
Santeeisweird9 <57926741+Santeeisweird9@users.noreply.github.com>
2022-06-08 17:42:53 -0700

    feat: scratcher command

--- src/commands/gamble.ts
+++ src/commands/gamble.ts
@@ -1,6 +1,6 @@
 import { CommandInteraction } from "discord.js";
 import { Discord, Slash, SlashChoice, SlashGroup, SlashOption } from "discordx";
-import { flipCoin } from "../util/economyUtil.js";
+import { flipCoin, scratch } from "../util/economyUtil.js";
 
 @Discord()
 @SlashGroup({ name: "gamble", description: "Gamble your life savings away." })
@@ -19,6 +19,24 @@ export class Gamble {
         //     if (choice.toLowerCase() !== "tails") return interaction.reply("Please pick between heads or tails.");
         // }
         flipCoin(interaction.user.id, amount, choice).then(e => {
+            return interaction.reply({ embeds: [e] });
+        }).catch(e => {
+            return interaction.reply(e);
+        })
+    }
+
+    @Slash("scratch", { description: "Do some scratchers." })
+    @SlashGroup("gamble")
+    async scratch (
+        @SlashChoice("$5", "$25", "$50", "$100", "$250", "$500")
+        @SlashOption("choice", { description: "What scratcher to get (higher value for higher prizes)." })
+        choice: string,
+        interaction: CommandInteraction
+    ) {
+        // if (choice.toLowerCase() !== "heads") {
+        //     if (choice.toLowerCase() !== "tails") return interaction.reply("Please pick between heads or tails.");
+        // }
+        scratch(interaction.user.id, choice).then(e => {
             return interaction.reply({ embeds: [e] });
         }).catch(e => {
             return interaction.reply(e);
